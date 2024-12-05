33285e662940888b2ec76efdc9968449c9221ab6
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-12-05 10:51:21 -0300

    terceiro

--- Emanuel/Program.cs
+++ Emanuel/Program.cs
@@ -1,5 +1,7 @@
 using Emanuel.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
@@ -14,5 +16,47 @@ app.MapPost("/api/funcionario/cadastrar", ([FromBody] Funcionario funcionario,
     ctx.SaveChanges();
     return Results.Created($"/produto/{funcionario.Id}", funcionario);
 });
+
+app.MapGet("/api/funcionario/listar", ([FromServices] AppDataContext ctx) =>
+{
+    return Results.Ok(ctx.Funcionarios.ToList());
+});
+
+
+app.MapPost("/api/folha/cadastrar",([FromBody] Folha folha,
+[FromServices] AppDataContext ctx) =>
+{
+    Funcionario? funcionario =
+    ctx.Funcionarios.Find(folha.FuncionarioId);
+
+    if(funcionario is null)
+    return Results.NotFound("Funcionario nao encontrado");
+
+    folha.Funcionario = funcionario;
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.Run();
