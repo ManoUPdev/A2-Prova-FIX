416ab63b1f2a294053b5f7a509211dbb9bfcb113
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-12-11 19:42:11 -0300

    versao final

--- API/Models/AppDataContext.cs
+++ API/Models/AppDataContext.cs
@@ -4,7 +4,7 @@ namespace API.Models;
 
 public class AppDataContext : DbContext
 {
-   public DbSet<Tarefa> Tarefas { get; set ;}
+    public DbSet<Tarefa> Tarefas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
